fda8b92f1c0eb310a88dddb5804e3de91b88d25d
Kunal <kunaldeshmukh2503@gmail.com>
2020-03-21 07:01:59 -0700

    Use case 2

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,3 +5,9 @@ echo "Welcome to Employee Wage Computation Program"
 
 
 [ $((RANDOM%2)) -eq 1 ] && echo Employee Present || echo Employee Absent
+
+
+wagePerHour=20
+fullDayWork=8
+dailyWage=$((wagePerHour*fullDayWork))
+echo "Daily Employee Wage is :"$dailyWage
